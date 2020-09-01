eba756d5fd8edf4c7efa29151f6eb2bbc334a4fb
Alexa Gamil <alexis_gamil99@yahoo.com>
2020-09-01 21:11:06 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -48,10 +48,10 @@ def add_characters
          ]
       },
      
-    :additional_characters =>
+    epic_tragedy[:additional_characters] =
   [ 
-     { name : "Prince Escalus" }, 
-     { name : "Apothecary"} 
+     { :name => "Prince Escalus" }, 
+     { :name => "Apothecary" } 
   ]
   
    }
